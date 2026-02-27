b65cd79f45fb4f5d82b807e67a58447c5c5dc2c2
Purnima <121791091+beansonli@users.noreply.github.com>
2026-02-27 12:05:07 +0530

    Update lab1_q1.cpp

--- DSA/lab1_q1.cpp
+++ DSA/lab1_q1.cpp
@@ -1,3 +1,15 @@
+/******************************************************************************
+Develop a Menu driven program to demonstrate the following operations of Arrays
+——MENU——-
+1.CREATE
+2.DISPLAY
+3.INSERT
+4.DELETE
+5.LINEAR SEARCH
+6.EXIT
+*******************************************************************************/
+
+
 #include <iostream>
 using namespace std;
 
@@ -75,3 +87,4 @@ int main() {
         }
     }
 }
+
